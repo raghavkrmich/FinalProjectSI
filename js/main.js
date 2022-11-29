0dc53ce3179040a18bc10cb12017e1fc43391901
dmaslia <dmaslia@umich.edu>
2022-11-29 18:35:33 -0500

    changes

--- js/main.js
+++ js/main.js
@@ -2,14 +2,34 @@ window.addEventListener("load", function(){
     setTimeout(
         function open(event){
             document.querySelector(".popup").style.display = "block";
-        },
-        1000
+        }
     )
 });
-document.querySelector("#close").addEventListener("click", function(){
+document.querySelector("#close").onclick = function(){
     document.querySelector(".popup").style.display = "none";
+};
+
+document.querySelector("#close2").addEventListener("click", function(){
+    document.querySelector(".popup2").style.display = "none";
 });
 
-window.addEventListener("dblclick", function(){
+document.querySelector("#btn").onclick = function(){
+    document.querySelector(".popup2").style.display = "block";
+}
+// var videos = document.getElementsByTagName("video");
+// for(let i = 0, len = videos.length; i < len; i++){
+//     videos[i].onclick = function(){
+//         videos[i].paused ? videos[i].play() : videos[i].pause();
+//     }
+// }
+
+
+// $('#homeform').submit(function(){
+//     return false;
+// })
+
+/*window.addEventListener("dblclick", function(){
     this.alert(new Date())
-})
+})*/
+
+//document.querySelector("#btn").onclick = function(){alert('test')};
