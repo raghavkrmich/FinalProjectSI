29d4bb2d6d0569aebde82baa57a0ba821214184a
dmaslia <dmaslia@umich.edu>
2022-12-05 21:45:20 -0500

    fixed errors

---
+++ js/pages.js
@@ -0,0 +1,26 @@
+//var counter = 0;
+document.querySelector("#close2").addEventListener("click", function(){
+    document.querySelector(".popup2").style.display = "none";
+});
+
+document.querySelector("#btn").onclick = function(){
+    document.querySelector(".popup2").style.display = "block";
+}
+
+// var videos = document.getElementsByTagName("video");
+// for(let i = 0, len = videos.length; i < len; i++){
+//     videos[i].onclick = function(){
+//         videos[i].paused ? videos[i].play() : videos[i].pause();
+//     }
+// }
+
+
+// $('#homeform').submit(function(){
+//     return false;
+// })
+
+/*window.addEventListener("dblclick", function(){
+    this.alert(new Date())
+})*/
+
+//document.querySelector("#btn").onclick = function(){alert('test')};
